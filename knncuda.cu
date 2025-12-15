d39083c77f52443dff0a67e7c1b72bf4b6c48553
Oier Elortondo <o.elortondo@gmail.com>
2025-12-15 18:33:02 +0100

    knncuda.cu programaren lehen bertsioa

--- knncuda.cu
+++ knncuda.cu
@@ -6,15 +6,17 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <cuda_runtime.h>
 
 
 #define VOCAB_SIZE 10000     // Hitz kopuru maximoa -- Maximo num. de palabras
 #define EMB_SIZE 50  	     // Embedding-en kopurua hitzeko -- Nº de embedding-s por palabra
+#define BLTAM 128
 
 
 // Bi bektoreen arteko biderketa eskalarra kalkulatzeko funtzioa
 // Función para calcular el producto escalar entre dos vectores
-double dot_product(float* a, float* b, int size) {
+__device__ double dot_product(float* a, float* b, int size) {
     double result = 0;
     for (int i = 0; i < size; i++) {
         result += a[i] * b[i];
@@ -24,7 +26,7 @@ double dot_product(float* a, float* b, int size) {
 
 // Bi bektoreen arteko norma (magnitudea) kalkulatzeko funtzioa
 // Función para calcular la norma (magnitud) de un vector
-float magnitude(float* vec, int size) {
+__device__ float magnitude(float* vec, int size) {
     float sum = 0;
     for (int i = 0; i < size; i++) {
         sum += vec[i] * vec[i];
@@ -34,7 +36,7 @@ float magnitude(float* vec, int size) {
 
 // Bi bektoreen arteko kosinu antzekotasuna kalkulatzeko funtzioa
 // Función para calcular la similitud coseno entre dos vectores
-float cosine_similarity(float* vec1, float* vec2, int size) {
+__device__ float cosine_similarity(float* vec1, float* vec2, int size) {
     float mag1, mag2;
     
     mag1 = magnitude(vec1, size);
@@ -44,8 +46,8 @@ float cosine_similarity(float* vec1, float* vec2, int size) {
 
 
 // kNN hitz guztietarako -- kNN para todas las palabras
-void knn_complet(float *words, int numwords, float *similarities) {
-    int i,j;
+__global__ void knn_complet(float *words, int numwords, float *similarities) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
     
 /******************************************************************
     // Hitz bakoitzak beste guztien duen antzekotasuna kalkulatu 
@@ -53,14 +55,12 @@ void knn_complet(float *words, int numwords, float *similarities) {
 
     //    OSATZEKO - PARA COMPLETAR
 ******************************************************************/
-  for (i = 0; i < numwords; i++) {
-    for (j = 0; j < numwords; j++) {
-      float *vec_i = &words[i * EMB_SIZE];
-      float *vec_j = &words[j * EMB_SIZE];
-
+  if(i<numwords){
+    float *vec_i = &words[i/numwords * EMB_SIZE];
+    for (int j = 0; j < numwords; j++) {
+      float *vec_j = &words[i%numwords * EMB_SIZE];
       float sim = cosine_similarity(vec_i, vec_j, EMB_SIZE);
-
-      similarities[i * numwords + j] = sim;
+      similarities[i] = sim;
     }
   }
 }
@@ -73,10 +73,11 @@ int main(int argc, char *argv[])
     float 	*words;
     FILE    	*f1, *f2;
     float 	*similarities;
-    
+    //GPUko memoria esleipenak
+    float *d_words, *d_similarities;
+
     struct timespec  t0, t1;
     double tej;
-
 
    if (argc < 3) {
      printf("Deia-Llamada: knn embeddins.dat similarities.dat [numwords]\n");
@@ -112,7 +113,20 @@ int main(int argc, char *argv[])
     fscanf (f1, "%f", &(words[i*EMB_SIZE+j]));
    }
   }
+  // Bektoreen tamainak bytetan
+  size_t words_size = numwords * emb_size * sizeof(float);
+  size_t similarities_size = numwords * numwords * sizeof(float);
+
+  cudaMalloc((void**)&d_words, words_size);
+  cudaMalloc((void**)&d_similarities, similarities_size);
+
+  //Datuak Hostatik Gailura kopiatu
+  cudaMemcpy(d_words, words, words_size, cudaMemcpyHostToDevice);
+
   printf ("Embeddingak irakurrita\n");
+
+  //Bloke kopurua kalkulatu
+  int BLKOP = (numwords + BLTAM - 1) / BLTAM;
   
   printf("Hitz guztien auzokideak kalkulatzera zoaz\n");
   clock_gettime (CLOCK_REALTIME, &t0);
@@ -122,11 +136,14 @@ int main(int argc, char *argv[])
     
     //    OSATZEKO - PARA COMPLETAR
 ******************************************************************/
-  knn_complet(words, numwords, similarities);
+  knn_complet<<<BLKOP, BLTAM>>>(d_words, numwords, d_similarities);
   clock_gettime (CLOCK_REALTIME, &t1);
+
+  //Emaitzak Gailutik Hostara kopiatu
+  cudaMemcpy(similarities, d_similarities, similarities_size, cudaMemcpyDeviceToHost);
    
   tej = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / (double)1e9;
-  printf("\n Tej. (serie) = %1.3f ms\n\n", tej*1000);
+  printf("\n Tej. (paraleloan) = %1.3f ms\n\n", tej*1000);
 
 // Idatzi antzekotasunak similarities fitxategietan -- Escribe las similitudes en el fichero similarities
   f2 = fopen (argv[2], "w");
@@ -147,8 +164,12 @@ int main(int argc, char *argv[])
   fclose (f1);
   fclose (f2);
 
+  //CPU-ko memoria askatu
   free(words);
   free(similarities); 
+  //GPU-ko memoria askatu
+  cudaFree(d_words);
+  cudaFree(d_similarities);
 
   return 0;
 }
